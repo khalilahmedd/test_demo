025e43e9e9abb93534182c018d7e4a5fec5d5eee
ahmed khalil <105124059+khalilahmedd@users.noreply.github.com>
2023-10-18 19:03:05 +0300

    Create newpipeline.groovy

---
+++ newpipeline.groovy
@@ -0,0 +1,17 @@
+def test (){
+echo "this is test step "
+    
+}
+def archive(){
+
+    echo "this is archive step "
+}
+def package(){
+
+    echo "this is archive step "
+}
+def build(){
+echo "this is the archive stage "
+echo "this is the deploy version is ${params.Version}"
+
+}
